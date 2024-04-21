73c4134979f97e587fc7633542e582f6758ccb97
phu <phuduong@csus.edu>
2024-04-21 13:32:50 -0700

    Change login button and added example image to graph

--- myapp-react/vite/src/pages/dashboard.jsx
+++ myapp-react/vite/src/pages/dashboard.jsx
@@ -1,6 +1,7 @@
 
 import DashboardNav from "../components/dashboardNav";
 import styles from "../cssPages/dashboardPage.module.css"
+import samplePlot from "../assets/samplePlot.png"
 
 function Dashboard() {
     return(
@@ -9,9 +10,14 @@ function Dashboard() {
                 <div className={`${styles.dashboardNavBox}`}>
                     <DashboardNav/>
                     <div className={`${styles.dashboardNavLeft}`}>
-                        {/* Just for testing purpose if anyone want to delete you can */}
-                        <h1 className={`${styles.dashboardTitle}`}>DashBoard</h1>
-                        <p className={`${styles.dashboardText}`}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        
+                        <div>
+                            <h1 className={`${styles.dashboardTitle}`}>Sample PLot</h1>
+                            <p className={`${styles.dashboardText}`}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        </div>
+                        <div className={`${styles.plotBox}`}>
+                            <img src={samplePlot} className={`${styles.imageSample}`} alt="Sample plot image"/>
+                        </div>
                     </div>
                 </div>
             </div>
