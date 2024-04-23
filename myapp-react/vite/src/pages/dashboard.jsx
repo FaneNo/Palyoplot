d79be1af3f0613272ce70183573c8cd8991db983
phu <phuduong@csus.edu>
2024-04-23 00:58:58 -0700

    move edit profile to another page and improve UI

--- myapp-react/vite/src/pages/dashboard.jsx
+++ myapp-react/vite/src/pages/dashboard.jsx
@@ -19,26 +19,7 @@ function Dashboard() {
         {id: 3, title: "Graph 3"}
     ];
 
-    const [userInfo, setUserInfo] = useState ({
-        //place holder for user info, will grab from server in future
-        username: "HerkyHornet",
-        password: "Herky1234",
-        email: "herkyhornet@csus.edu"
 
-    });
-
-    const infoUpdate = (event) => {
-        //handles form submission for updates
-        event.preventDefault();
-    }
-
-    const infoChange = (event) => {
-        const {name, value} = event.target;
-        setUserInfo((prevInfo) => ({
-            ...prevInfo,
-            [name]: value
-        }));
-    };
 
     return(
         <>
@@ -47,64 +28,25 @@ function Dashboard() {
                     <DashboardNav/>
                     <div className={`${styles.dashboardNavLeft}`}>
                         {/* Just for testing purpose if anyone want to delete you can */}
-                        <h1 className={`${styles.dashboardTitle}`}>DashBoard</h1>
-                        <p className={`${styles.dashboardText}`}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-
-                        <button type= "Upload" className={styles.dashboardUpload}>
-                            Upload CSV File
-                        </button>
-
-                        <h1 className={styles.dashboardRecentText}>
-                            Recent Graphs
-                        </h1>
-
-                        <div className={styles.dashboardText}>
-                            {graphData.map((graph) => (
-                                <button key={graph.id} className={styles.dashboardGraphPrev} onClick={() => handleGraphClick(graph.id)}>
-                                    <img src={"https://user-images.githubusercontent.com/6562690/54934415-b4d25b80-4edb-11e9-8758-fb29ada50499.png"} alt={`Preview of ${graph.title}`} />
-                                    <span>{graph.title}</span>
-                                </button>
-                            ))}
+        
+                        <div>
+                            
+                            <button type= "Upload" className={styles.dashboardUpload}>
+                                Upload CSV File
+                            </button>
+                            <h1 className={styles.dashboardRecentText}>
+                                Recent Graphs
+                            </h1>
+                            <div className={styles.dashboardText}>
+                                {graphData.map((graph) => (
+                                    <button key={graph.id} className={styles.dashboardGraphPrev} onClick={() => handleGraphClick(graph.id)}>
+                                        <img src={"https://user-images.githubusercontent.com/6562690/54934415-b4d25b80-4edb-11e9-8758-fb29ada50499.png"} alt={`Preview of ${graph.title}`} />
+                                        <span>{graph.title}</span>
+                                    </button>
+                                ))}
+                            </div>
                         </div>
-
-                        <div className={styles.dashboardAccContainer}>
-                            <h2 className={styles.dashboardAccInfoTitle}>Account Info</h2>
-                            <form onSubmit={infoUpdate} className={styles.dashboardAccForm}>
-                                <div className={styles.dashboardInput}>
-                                    <label htmlFor="username">Username:</label>
-                                    <input
-                                        id="username"
-                                        type="text"
-                                        name="username"
-                                        value={userInfo.username}
-                                        onChange={infoChange}
-                                    />
-                                </div>
-                                <div className={styles.dashboardInput}>
-                                    <label htmlFor="password">Password:</label>
-                                    <input
-                                        id="password"
-                                        type="password"
-                                        name="password"
-                                        value={userInfo.password}
-                                        onChange={infoChange}
-                                    />
-                                </div>
-                                <div className={styles.dashboardInput}>
-                                    <label htmlFor="email">Email:</label>
-                                    <input
-                                        id="email"
-                                        type="email"
-                                        name="email"
-                                        value={userInfo.email}
-                                        onChange={infoChange}
-                                    />
-                                </div>
-                                <button type="submit" className={styles.dashboardUpdate}>Update</button>
-                            </form>
-                        </div>
-
-
+                        
 
                          {/*<div>
                             <button type="upload" className={styles.uploadButton}>Upload</button>
@@ -123,4 +65,4 @@ function Dashboard() {
 }
 
 
-export default Dashboard
+export default Dashboard;
