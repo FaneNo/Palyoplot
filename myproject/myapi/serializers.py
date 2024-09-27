2c9095f3e97a2aa26ebe35b3a55015cca2cb6a88
FaneNo <FaneNo@users.noreply.github.com>
2024-09-26 22:41:11 -0700

    fix bug with serializers

--- myproject/myapi/serializers.py
+++ myproject/myapi/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import GraphImage
 
 
 
--- myproject/myapi/urls.py
+++ myproject/myapi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import SaveGraphImageView, UserGraphImagesView
+
 
 urlpatterns = [
     path('hello-world/', views.hello_world, name='hello_world'),
