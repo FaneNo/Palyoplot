7505c8b715559b78757373499f20267493a498e7
Ironiciron <nicholas.bam.sanchez@gmail.com>
2024-11-02 15:50:02 -0700

    Recommented code in settings.py to fix the login issue temporarily

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -111,11 +111,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         # 'NAME': BASE_DIR / 'db.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'PORT': os.getenv('DB_PORT'),
         
         'NAME': 'palyoplot',
         'USER': 'csc190191',
